75729a6112611feee7f95927e90d95ec9188c81d
John Haiducek <jhaiduce@gmail.com>
2019-08-09 14:28:07 -0400

    Use for fortran array order when converting HP and HPH pointers into numpy arrays

--- enkf.py
+++ enkf.py
@@ -74,13 +74,13 @@ def lenkf_rsm(step,ind_p,ensemble_state,predictions,innovations,add_obs_err,loca
 
     @ffi.callback("void(*)(int,int,int,int,double*,double*)")
     def localization_cb(step,ind_p,dim_p,dim_obs,HP_p_ptr,HPH_ptr):
-        HP_p=ptr_to_array(HP_p_ptr,(dim_obs,dim_p))
+        HP_p=ptr_to_array(HP_p_ptr,(dim_obs,dim_p),order='F')
         HPH=ptr_to_array(HPH_ptr,(dim_obs,dim_obs))
         localization(step,ind_p,HP_p,HPH)
 
     @ffi.callback("void(*)(int,int,int,double*)")
     def add_obs_err_cb(step,ind_p,dim_obs,HPH_ptr):
-        HPH=ptr_to_array(HPH_ptr,(dim_obs,dim_obs))
+        HPH=ptr_to_array(HPH_ptr,(dim_obs,dim_obs),order='F')
         add_obs_err(step,ind_p,HPH)
 
     lib.lenkf_analysis_rsm(
