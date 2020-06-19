13302f43bc0483cd55efc905793badd0fde181d3
John Haiducek <jhaiduce@gmail.com>
2020-06-19 13:56:55 -0400

    Added tests to verify batch lengths and offsets

--- tests/batch_manager_tests_parallel.F90
+++ tests/batch_manager_tests_parallel.F90
@@ -34,6 +34,49 @@ contains
     end select
   end subroutine add_obs_err
 
+  subroutine test_batch_math()
+
+    type(assim_batch_manager)::batch_manager
+    type(dummy_model_interface)::model_interface
+    integer,parameter::n_observations=5
+    integer,parameter::state_size=100
+    integer,parameter::batch_size=7
+    integer,parameter::n_ensemble=15
+    real(kind=8),allocatable::local_batches(:,:,:)
+    integer,allocatable::local_batch_inds(:)
+    integer::rank,comm,comm_size,ierr,batch_length,batch_offset,ibatch,istep, &
+         n_batches,sum_batch_lengths,offset,last_offset
+    real(kind=8),parameter::forget=0.6
+
+    comm=mpi_comm_world
+
+    comm_size=10
+
+    model_interface=new_dummy_model(n_ensemble,n_observations,state_size,comm)
+
+    batch_manager=new_batch_manager(model_interface,n_ensemble,state_size,batch_size,mpi_comm_world)
+
+    n_batches=batch_manager%get_n_batches()
+    sum_batch_lengths=0
+    last_offset=0
+
+    do ibatch=1,n_batches
+       batch_length=batch_manager%get_batch_length(ibatch)
+       sum_batch_lengths=sum_batch_lengths+batch_length
+       batch_offset=batch_manager%get_batch_length(ibatch)
+       if(ibatch>1.and.batch_offset/=last_offset+batch_length) then
+          print *,'Offset incorrect for batch',ibatch,'. Expected',last_offset+batch_length,'got',batch_offset
+          error stop
+       end if
+    end do
+
+    if(sum_batch_lengths/=state_size) then
+       print *,'Sum of batch lengths was',sum_batch_lengths,' expected',state_size
+       error stop
+    end if
+
+  end subroutine test_batch_math
+
   subroutine test_empty_assimilator()
 
     type(assim_batch_manager)::batch_manager
@@ -126,6 +169,7 @@ program test_batch_manager
   implicit none
 
   call mpi_init()
+  call test_batch_math()
   call test_empty_assimilator()
   call mpi_finalize()
 end program test_batch_manager
