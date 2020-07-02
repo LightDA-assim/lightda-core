8df33e04023cd80a4a975c8665eeb543e73b58cc
John Haiducek <jhaiduce@gmail.com>
2020-07-02 15:01:09 -0400

    Moved commands to configure an HDF5 target into a function for reuse

--- tests/advect1d_model_interface_tests.f90
+++ tests/advect1d_model_interface_tests.f90
@@ -1,4 +1,63 @@
-program dummy_model_interface_tests
+module advect1d_model_interface_tests
+
+  use advect1d_assimilate_interfaces,ONLY:advect1d_interface
+  use random_integer, ONLY: randint
+  implicit none
+
+contains
+
+  subroutine test_localization()
+    type(advect1d_interface)::iface
+    integer::istep,subset_offset,subset_size,imodel,iobs1,iobs2,state_size,n_obs
+    integer,allocatable::obs_positions(:)
+    integer::i,model_pos,obs_pos1,obs_pos2
+    real::weight
+
+    istep=1
+
+    state_size=iface%get_state_size()
+    n_obs=iface%get_subset_obs_count(istep,0,state_size)
+
+    allocate(obs_positions(n_obs))
+
+    obs_positions=iface%get_obs_positions()
+
+    do i=1,100
+       imodel=randint(state_size)
+       iobs1=randint(n_obs)
+       iobs2=randint(n_obs)
+
+       model_pos=mod(imodel,state_size/2)
+       obs_pos1=obs_positions(iobs1)
+       obs_pos2=obs_positions(iobs2)
+
+       weight=iface%get_weight_obs_obs(istep,iobs1,iobs2)
+
+       if(obs_pos1/=obs_pos2 .and. weight>=1) then
+          print *,'Weight should be less than 1 for differently placed observations'
+          error stop
+       end if
+
+       weight=iface%get_weight_model_obs(istep,imodel,iobs1)
+       if(weight==1) then
+          print *,'Weight be less than 1 when model position is different from observation position'
+          error stop
+       end if
+
+       weight=iface%get_weight_model_obs(istep,obs_pos1,iobs1)
+
+       if(weight/=1) then
+          print *,'Weight should equal one when model position is the same as observing position'
+          error stop
+       end if
+
+    end do
+
+  end subroutine test_localization
+
+end module advect1d_model_interface_tests
+
+program test_advect1d_model_interface
   use model_interface_tests, ONLY: run_all
   use advect1d_assimilate_interfaces,ONLY:advect1d_interface,new_advect1d_interface
   use system_mpi
@@ -15,4 +74,4 @@ program dummy_model_interface_tests
 
   call run_all(model_interface)
   call mpi_finalize(ierr)
-end program dummy_model_interface_tests
+end program test_advect1d_model_interface
