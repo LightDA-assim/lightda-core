4ca83e2de35c6355edbdafe64a80aa8f64fd7bdf
John Haiducek <jhaiduce@gmail.com>
2020-07-02 17:09:31 -0400

    Subroutine to generate input files required by advect1d_interface

--- tests/advect1d_model_interface_tests.f90
+++ tests/advect1d_model_interface_tests.f90
@@ -2,14 +2,125 @@ module advect1d_model_interface_tests
 
   use advect1d_assimilate_interfaces,ONLY:advect1d_interface
   use random_integer, ONLY: randint
+  use hdf5
+
   implicit none
 
   integer, parameter::istep=1
 
 contains
 
-  subroutine test_localization()
-    type(advect1d_interface)::iface
+  subroutine write_observations(observations,obs_errors,obs_positions)
+    real(kind=8),intent(in)::observations(:),obs_errors(:)
+    integer,intent(in)::obs_positions(:)
+    character(len=50)::obs_filename
+    integer(HID_T)::h5file_h,dset_h,dataspace
+    integer(HSIZE_T)::data_block(2)
+    integer::ierr
+    character(len=10)::istepstr
+
+    data_block=(/1,size(observations)/)
+
+    ! Convert istep to string
+    write (istepstr,'(I10)') istep
+
+    ! Create parent directory
+    call execute_command_line('mkdir -p ensembles/'//adjustl(trim(istepstr)))
+
+    ! Set the HDF5 filename
+    write(obs_filename,"(A,I0,A)") &
+         'ensembles/',istep,'/observations.h5'
+
+    ! Delete any preexisting file
+    call execute_command_line('rm -f '//adjustl(trim(obs_filename)))
+
+    ! Create the file
+    call h5fcreate_f(obs_filename,H5F_ACC_TRUNC_F,h5file_h,ierr)
+
+    if(ierr/=0) error stop
+
+    ! Define a dataspace for the observations
+    call h5screate_simple_f(1,(/int(size(observations),8)/),dataspace,ierr)
+
+    if(ierr/=0) error stop
+
+    ! Create the observations dataset
+    call h5dcreate_f(h5file_h,'observations',H5T_IEEE_F64LE, &
+         dataspace,dset_h,ierr)
+
+    if(ierr/=0) error stop
+
+    ! Write the data
+    call h5dwrite_f(dset_h,H5T_NATIVE_DOUBLE,observations,data_block,ierr,file_space_id=dataspace)
+
+    if(ierr/=0) error stop
+
+    ! Close the dataset
+    call h5dclose_f(dset_h,ierr)
+
+    if(ierr/=0) error stop
+
+    ! Create the obs_errors dataset
+    call h5dcreate_f(h5file_h,'obs_errors',H5T_IEEE_F64LE, &
+         dataspace,dset_h,ierr)
+
+    if(ierr/=0) error stop
+
+    ! Write the data
+    call h5dwrite_f(dset_h,H5T_NATIVE_DOUBLE,obs_errors,data_block,ierr,file_space_id=dataspace)
+
+    if(ierr/=0) error stop
+
+    ! Close the dataset
+    call h5dclose_f(dset_h,ierr)
+
+    ! Create the obs_positions dataset
+    call h5dcreate_f(h5file_h,'obs_positions',H5T_NATIVE_INTEGER, &
+         dataspace,dset_h,ierr)
+
+    if(ierr/=0) error stop
+
+    ! Write the data
+    call h5dwrite_f(dset_h,H5T_NATIVE_INTEGER,obs_positions,data_block,ierr,file_space_id=dataspace)
+
+    if(ierr/=0) error stop
+
+    ! Close the dataset
+    call h5dclose_f(dset_h,ierr)
+
+    ! Close the dataspace
+    call h5sclose_f(dataspace,ierr)
+
+    ! Close the file
+    call h5fclose_f(h5file_h,ierr)
+
+  end subroutine write_observations
+
+  subroutine generate_inputs(iface)
+
+    class(advect1d_interface)::iface
+    real(kind=8),allocatable::observations(:),obs_errors(:)
+    integer,allocatable::obs_positions(:)
+    integer::state_size,n_obs,i
+
+    state_size=iface%get_state_size()
+    n_obs=iface%get_subset_obs_count(istep,0,state_size)
+
+    allocate(observations(n_obs),obs_errors(n_obs),obs_positions(n_obs))
+
+    call random_number(observations)
+    call random_number(obs_errors)
+
+    do i=1,n_obs
+       obs_positions(i)=randint(state_size/2)
+    end do
+
+    call write_observations(observations,obs_errors,obs_positions)
+
+  end subroutine generate_inputs
+
+  subroutine test_localization(iface)
+    class(advect1d_interface)::iface
     integer::istep,subset_offset,subset_size,imodel,iobs1,iobs2,state_size,n_obs
     integer,allocatable::obs_positions(:)
     integer::i,model_pos,obs_pos1,obs_pos2
