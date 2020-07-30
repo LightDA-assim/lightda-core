a1e022a8968b74e35de724d2c6892eee8c2ec470
John Haiducek <jhaiduce@gmail.com>
2020-07-30 16:33:40 -0400

    Moved enable_language(Fortran), find_package(MPI), MPI-related compile definitions, and find_package(HDF5) to lenkf_functions.cmake

--- cmake/modules/lenkf_functions.cmake
+++ cmake/modules/lenkf_functions.cmake
@@ -1,5 +1,19 @@
 cmake_minimum_required(VERSION 3.10)
 
+enable_language(Fortran)
+
+find_package(MPI REQUIRED)
+
+if(MPI_Fortran_HAVE_F90_MODULE)
+  add_definitions(-DHAVE_MPI_F90_MODULE)
+endif()
+
+if(MPI_Fortran_HAVE_F08_MODULE)
+  add_definitions(-DHAVE_MPI_F08_MODULE)
+endif()
+
+find_package(HDF5 COMPONENTS Fortran REQUIRED)
+
 function(configure_mpi_target target)
   target_compile_definitions(${target} PRIVATE ${MPI_Fortran_COMPILE_DEFINITIONS})
   target_compile_options(${target} PRIVATE ${MPI_Fortran_COMPILE_OPTIONS})
