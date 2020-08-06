df4dc09baccddb8864060d89f127b903c9c73c73
John Haiducek <jhaiduce@gmail.com>
2020-08-06 14:07:23 -0400

    Added tests of the tree module

--- tests/util_tests.f90
+++ tests/util_tests.f90
@@ -1,5 +1,6 @@
 module util_tests
   use util
+  use tree
   implicit none
 contains
   subroutine test_append_array
@@ -31,6 +32,85 @@ contains
     end if
 
   end subroutine test_append_array
+
+  subroutine test_tree
+
+    type(node),target::root
+        !! Root node
+    type(node),target::insertNode1,insertNode2,insertNode4,insertNode5,insertNode6
+        !! Nodes to be inserted into tree
+    type(node),pointer::foundNode
+        !! Node found in tree
+    real(kind=8),target::data(6)
+        !! Array to be stored in tree
+
+    ! Fill data array
+    data=(/1,2,3,4,5,6/)
+
+    ! Assign data elements and keys to nodes
+
+    root%key=3
+    root%data=>data(3)
+
+    insertNode1%key=1
+    insertNode1%data=>data(1)
+
+    insertNode2%key=2
+    insertNode1%data=>data(2)
+
+    insertNode4%key=4
+    insertNode4%data=>data(4)
+
+    insertNode5%key=5
+    insertNode5%data=>data(5)
+
+    insertNode6%key=6
+    insertNode6%data=>data(6)
+
+    ! Insert nodes into tree
+    call root%insert(insertNode5)
+    call root%insert(insertNode4)
+    call root%insert(insertNode1)
+    call root%insert(insertNode2)
+    call root%insert(insertNode6)
+
+    foundNode=>root%find(1)
+    if(.not.associated(foundNode,insertNode1)) then
+       print *,'Error: Wrong node returned from node%find'
+       error stop
+    end if
+
+    foundNode=>root%find(2)
+    if(.not.associated(foundNode,insertNode2)) then
+       print *,'Error: Wrong node returned from node%find'
+       error stop
+    end if
+
+    foundNode=>root%find(3)
+    if(.not.associated(foundNode,root)) then
+       print *,'Error: Wrong node returned from node%find'
+       error stop
+    end if
+
+    foundNode=>root%find(4)
+    if(.not.associated(foundNode,insertNode4)) then
+       print *,'Error: Wrong node returned from node%find'
+       error stop
+    end if
+
+    foundNode=>root%find(5)
+    if(.not.associated(foundNode,insertNode5)) then
+       print *,'Error: Wrong node returned from node%find'
+       error stop
+    end if
+
+    foundNode=>root%find(6)
+    if(.not.associated(foundNode,insertNode6)) then
+       print *,'Error: Wrong node returned from node%find'
+       error stop
+    end if
+
+  end subroutine test_tree
 end module util_tests
 
 program test_util
