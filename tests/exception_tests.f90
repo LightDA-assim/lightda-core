f66f6a96b79c02ca96942c4c438832fee81616cb
John Haiducek <jhaiduce@gmail.com>
2020-08-14 12:06:04 -0400

    Moved exception test routines into a separate source code file compiled into a library to reduce code duplication

---
+++ tests/exception_tests.f90
@@ -0,0 +1,101 @@
+module exception_tests
+
+  implicit none
+
+contains
+
+  subroutine test_no_throw(status)
+    !! Test procedure that can throw an error, but doesn't
+
+    use exceptions, ONLY: error_status
+
+    ! Arguments
+    class(error_status),allocatable,optional::status
+        !! Error status
+
+  end subroutine test_no_throw
+
+  subroutine test_throw(status)
+    !! Test procedure that throws an exception and exits
+
+    use exceptions, ONLY: throw, new_exception, error_status
+
+    ! Arguments
+    class(error_status),allocatable,optional::status
+        !! Error status
+
+    call throw(status,new_exception("An error occurred"))
+    return
+
+  end subroutine test_throw
+
+  subroutine test_throw_with_procname(status)
+    !! Test procedure that throws an exception with the procedure attribute set
+    !! and exits
+
+    use exceptions, ONLY: throw, new_exception, error_status
+
+    ! Arguments
+    class(error_status),allocatable,optional::status
+        !! Error status
+
+    call throw(status,new_exception("An error occurred","test_throw_with_procname"))
+    return
+
+  end subroutine test_throw_with_procname
+
+  subroutine test_fail()
+    !! Test function that calls an exception-throwing procedure and ignores the
+    !! exception explicitly, triggering a fatal error.
+
+    use exceptions, ONLY: exception,error_status
+
+    class(error_status),allocatable::status
+        !! Error status
+
+    call test_throw(status)
+
+    select type(status)
+    class is(exception)
+       print *,"Ignored error:",status%message
+    end select
+
+  end subroutine test_fail
+
+  subroutine test_catch()
+    !! Test procedure that calls exception-throwing procedures and handles the
+    !! exceptions
+
+    use exceptions, ONLY: exception,error_status
+
+    class(error_status),allocatable::status
+        !! Error status
+
+    call test_no_throw()
+
+    call test_no_throw(status)
+
+    select type(status)
+    class is(exception)
+       print *,"Handled error:",status%as_string()
+       status%handled=.true.
+    end select
+
+    call test_throw(status)
+
+    select type(status)
+    class is(exception)
+       print *,"Handled error:",status%as_string()
+       status%handled=.true.
+    end select
+
+    call test_throw_with_procname(status)
+
+    select type(status)
+    class is(exception)
+       print *,"Handled error:",status%as_string()
+       status%handled=.true.
+    end select
+  end subroutine test_catch
+
+end module exception_tests
--- tests/test_exception_fail.f90
+++ tests/test_exception_fail.f90
@@ -1,46 +1,6 @@
-module test_exception_fail
-
-  implicit none
-
-contains
-
-  subroutine test_throw(status)
-    !! Test function that throws an exception and exits
-
-    use exceptions, ONLY: throw, new_exception, error_status
-
-    ! Arguments
-    class(error_status),allocatable,optional::status
-        !! Error status
-
-    call throw(status,new_exception("An error occurred"))
-    return
-
-  end subroutine test_throw
-
-  subroutine test_fail()
-    !! Test function that calls an exception-throwing procedure and ignores the
-    !! exception, triggering an fatal error.
-
-    use exceptions, ONLY: exception,error_status
-
-    class(error_status),allocatable::status
-        !! Error status
-
-    call test_throw(status)
-
-    select type(status)
-    class is(exception)
-       print *,"Ignored error:",status%message
-    end select
-
-  end subroutine test_fail
-
-end module test_exception_fail
-
 program test_exception
 
-  use test_exception_fail, ONLY: test_fail
+  use exception_tests, ONLY: test_fail
 
   call test_fail()
 
