af5b0a814c36f03b53ffe5037876c209a6510c8b
John Haiducek <jhaiduce@gmail.com>
2020-08-18 13:18:31 -0400

    Check output of exception tests in addition to return codes

---
+++ tests/test_and_check_regex.cmake
@@ -0,0 +1,49 @@
+# Run the command
+execute_process(COMMAND ${CMD}
+  RESULT_VARIABLE CMD_RESULT
+  OUTPUT_VARIABLE OUTPUT
+  ERROR_VARIABLE OUTPUT)
+
+message("${OUTPUT}")
+
+# Check whether OUTPUT matches PASS_PATTERN
+if(DEFINED PASS_PATTERN)
+  string(REGEX MATCH ${PASS_PATTERN} PASS_MATCH ${OUTPUT})
+endif()
+
+# Check whether OUTPUT matches FAIL_PATTERN
+if(DEFINED FAIL_PATTERN AND OUTPUT)
+  string(REGEX MATCH "${FAIL_PATTERN}" FAIL_MATCH ${OUTPUT})
+endif()
+
+if(${WILL_FAIL})
+
+  message("${CMD} returned ${CMD_RESULT}")
+
+  if(NOT ${CMD_RESULT})
+    message(FATAL_ERROR "${CMD} should have failed, but didn't")
+  endif()
+
+  if(DEFINED FAIL_PATTERN AND NOT FAIL_MATCH)
+    message(FATAL_ERROR "${CMD} output did not match pattern \"${FAIL_PATTERN}\"")
+  endif()
+
+  if(DEFINED PASS_PATTERN AND PASS_MATCH)
+    message(FATAL_ERROR "${CMD} output matched pattern ${PASS_MATCH}")
+  endif()
+
+else()
+
+  if(${CMD_RESULT})
+    message(FATAL_ERROR "Error running ${CMD}")
+  endif()
+
+  if(DEFINED FAIL_PATTERN AND FAIL_MATCH)
+    message(FATAL_ERROR "${CMD} output matched pattern ${FAIL_MATCH}")
+  endif()
+
+  if(DEFINED PASS_PATTERN AND NOT PASS_MATCH)
+    message(FATAL_ERROR "${CMD} output did not match pattern ${PASS_MATCH}")
+  endif()
+
+endif()
