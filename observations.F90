7796de8e236d5b078ca0bd80f7c4b15cada42e35
John Haiducek <jhaiduce@gmail.com>
2020-09-10 16:50:20 -0400

    Added get_size and get_values procedures to base observation type

--- observations.F90
+++ observations.F90
@@ -1,7 +1,51 @@
 module observations
+
   implicit none
 
   type, abstract::observation_set
+   contains
+     procedure(get_size), deferred::get_size
+     procedure(get_values), deferred::get_values
   end type observation_set
+
+  abstract interface
+
+     function get_size(this,status) result(size)
+
+       use exceptions, ONLY: error_status
+
+       import observation_set
+
+       ! Arguments
+       class(observation_set)::this
+           !! Observation set
+       class(error_status), intent(out), allocatable, optional::status
+           !! Error status
+
+       ! Result
+       integer::size
+           !! Number of observation values in set
+
+     end function get_size
+
+     function get_values(this,status) result(values)
+
+       use exceptions, ONLY: error_status
+
+       import observation_set
+
+       ! Arguments
+       class(observation_set)::this
+           !! Observation set
+       class(error_status), intent(out), allocatable, optional::status
+           !! Error status
+
+       ! Result
+       real(kind=8), allocatable::values(:)
+           !! Observation values
+
+     end function get_values
+
+  end interface
 
 end module observations
