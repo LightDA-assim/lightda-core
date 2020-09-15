ba28cb8dccfe85ababc04059ce5267b20655cc60
John Haiducek <jhaiduce@gmail.com>
2020-09-15 13:06:07 -0400

    Added get_errors to base observation_set type

--- observations.F90
+++ observations.F90
@@ -6,6 +6,7 @@ module observations
    contains
      procedure(get_size), deferred::get_size
      procedure(get_values), deferred::get_values
+     procedure(get_values), deferred::get_errors
   end type observation_set
 
   abstract interface
@@ -45,6 +46,26 @@ module observations
            !! Observation values
 
      end function get_values
+
+     function get_errors(this,status) result(errors)
+
+       !! Get errors (uncertainties) associated with the observations
+
+       use exceptions, ONLY: error_status
+
+       import observation_set
+
+       ! Arguments
+       class(observation_set)::this
+           !! Observation set
+       class(error_status), intent(out), allocatable, optional::status
+           !! Error status
+
+       ! Result
+       real(kind=8), allocatable::errors(:)
+           !! Observation errors
+
+     end function get_errors
 
   end interface
 
