e8c1de8b5ba278338b0237994308ccfcd4f841c5
John Haiducek <jhaiduce@gmail.com>
2020-09-22 12:36:04 -0400

    Updated advect1d_model_interface_tests so it works with new observation and forward operator interfaces

--- tests/advect1d_model_interface_tests.f90
+++ tests/advect1d_model_interface_tests.f90
@@ -3,6 +3,9 @@ module advect1d_model_interface_tests
   use advect1d_assimilate_interfaces, ONLY: advect1d_interface
   use random_integer, ONLY: randint
   use hdf5
+  use exceptions, ONLY: error_status, throw, new_exception
+  use hdf5_exceptions, ONLY: new_hdf5_exception
+  use util, only: str
 
   implicit none
 
@@ -10,122 +13,147 @@ module advect1d_model_interface_tests
 
 contains
 
-  subroutine write_observations(observations, obs_errors, obs_positions)
-    real(kind=8), intent(in)::observations(:), obs_errors(:)
-    integer, intent(in)::obs_positions(:)
-    character(len=50)::obs_filename
-    integer(HID_T)::h5file_h, dset_h, dataspace
-    integer(HSIZE_T)::data_block(2)
-    integer::ierr
-    character(len=10)::istepstr
-
-    data_block = (/1, size(observations)/)
-
-    ! Convert istep to string
-    write (istepstr, '(I10)') istep
-
-    ! Create parent directory
-    call execute_command_line('mkdir -p ensembles/'//adjustl(trim(istepstr)))
-
-    ! Set the HDF5 filename
-    write (obs_filename, "(A,I0,A)") &
-      'ensembles/', istep, '/observations.h5'
-
-    ! Delete any preexisting file
-    call execute_command_line('rm -f '//adjustl(trim(obs_filename)))
-
-    ! Create the file
-    call h5fcreate_f(obs_filename, H5F_ACC_TRUNC_F, h5file_h, ierr)
-
-    if (ierr /= 0) error stop
-
-    ! Define a dataspace for the observations
-    call h5screate_simple_f(1, (/int(size(observations), 8)/), dataspace, ierr)
-
-    if (ierr /= 0) error stop
-
-    ! Create the observations dataset
-    call h5dcreate_f(h5file_h, 'observations', H5T_IEEE_F64LE, &
-                     dataspace, dset_h, ierr)
-
-    if (ierr /= 0) error stop
-
-    ! Write the data
-    call h5dwrite_f(dset_h, H5T_NATIVE_DOUBLE, observations, &
-                    data_block, ierr, file_space_id=dataspace)
-
-    if (ierr /= 0) error stop
-
-    ! Close the dataset
-    call h5dclose_f(dset_h, ierr)
-
-    if (ierr /= 0) error stop
-
-    ! Create the obs_errors dataset
-    call h5dcreate_f(h5file_h, 'obs_errors', H5T_IEEE_F64LE, &
-                     dataspace, dset_h, ierr)
-
-    if (ierr /= 0) error stop
-
-    ! Write the data
-    call h5dwrite_f( &
-      dset_h, H5T_NATIVE_DOUBLE, obs_errors, data_block, &
-      ierr, file_space_id=dataspace)
-
-    if (ierr /= 0) error stop
-
-    ! Close the dataset
-    call h5dclose_f(dset_h, ierr)
-
-    ! Create the obs_positions dataset
-    call h5dcreate_f(h5file_h, 'obs_positions', H5T_NATIVE_INTEGER, &
-                     dataspace, dset_h, ierr)
-
-    if (ierr /= 0) error stop
-
-    ! Write the data
-    call h5dwrite_f(dset_h, H5T_NATIVE_INTEGER, obs_positions, &
-                    data_block, ierr, file_space_id=dataspace)
-
-    if (ierr /= 0) error stop
-
-    ! Close the dataset
-    call h5dclose_f(dset_h, ierr)
-
-    ! Close the dataspace
-    call h5sclose_f(dataspace, ierr)
-
-    ! Close the file
-    call h5fclose_f(h5file_h, ierr)
-
-  end subroutine write_observations
-
-  subroutine generate_inputs(iface, n_obs)
-
-    class(advect1d_interface)::iface
-    real(kind=8), allocatable::observations(:), obs_errors(:)
-    integer, allocatable::obs_positions(:)
-    integer::state_size, n_obs, i
-
-    state_size = iface%get_state_size(istep)
-
-    allocate (observations(n_obs), obs_errors(n_obs), obs_positions(n_obs))
-
-    call random_number(observations)
-    call random_number(obs_errors)
-
-    do i = 1, n_obs
-      obs_positions(i) = randint(state_size/2)
-    end do
-
-    call write_observations(observations, obs_errors, obs_positions)
-
-  end subroutine generate_inputs
-
   subroutine run_all_advect1d(iface)
     class(advect1d_interface)::iface
 
   end subroutine run_all_advect1d
+
+  function generate_ensemble(istep, state_size, n_ensemble) result(ensemble)
+
+    integer, allocatable::obs_positions(:)
+    integer, intent(in)::istep, state_size, n_ensemble
+    real(kind=8)::ensemble(n_ensemble, state_size)
+
+    call random_number(ensemble)
+
+  end function generate_ensemble
+
+  subroutine write_ensemble(istep, ensemble, status)
+
+    use iso_fortran_env, ONLY: output_unit
+
+    !! Write the ensemble to disk
+
+    ! Arguments
+    integer, intent(in)::istep
+        !! Iteration number
+    real(kind=8)::ensemble(:, :)
+        !! ensemble state
+    class(error_status), intent(out), allocatable, optional::status
+        !! Error status
+
+    integer::state_size, n_ensemble
+    character(len=80)::preassim_filename
+    integer(HID_T)::h5file_h, dset_h, dataspace, memspace
+    integer(HSIZE_T)::offset(2), count(2), stride(2), data_block(2)
+    logical::exists
+    integer::ierr
+
+    n_ensemble = size(ensemble, 1)
+    state_size = size(ensemble, 2)
+
+    stride = (/1, 1/)
+    offset = (/0, 0/)
+    count = (/1, 1/)
+    data_block = (/n_ensemble, state_size/)
+
+    call h5open_f(ierr)
+
+    if (ierr < 0) then
+      call throw(status, new_hdf5_exception(ierr, &
+                                            'HDF5 initialization error', &
+                                            procedure='write_ensemble'))
+      return
+    end if
+
+    ! Set the HDF5 filename
+    ! Set the HDF5 filename
+    write (preassim_filename, "(A,I0,A)") &
+      'ensembles/', istep, '/preassim.h5'
+
+    call system('mkdir -p ensembles/'//str(istep))
+
+    ! Create the file
+    call h5fcreate_f(preassim_filename, H5F_ACC_TRUNC_F, h5file_h, ierr)
+
+    if (ierr < 0) then
+      call throw(status, new_hdf5_exception(ierr, &
+                                            'HDF5 error creating output file', &
+                                            filename=preassim_filename, &
+                                            procedure='write_ensemble'))
+      return
+    end if
+
+    ! Define a dataspace
+    call h5screate_simple_f(2, data_block, dataspace, ierr)
+
+    if (ierr < 0) then
+      call throw(status, new_hdf5_exception(ierr, &
+                                            'HDF5 error creating dataspace', &
+                                            filename=preassim_filename, &
+                                            procedure='write_ensemble'))
+      return
+    end if
+
+    ! Create the dataset
+    call h5dcreate_f(h5file_h, 'ensemble_state', H5T_IEEE_F64LE, &
+                     dataspace, dset_h, ierr)
+
+    if (ierr < 0) then
+      call throw(status, new_hdf5_exception(ierr, &
+                                            'HDF5 error creating dataset', &
+                                            filename=preassim_filename, &
+                                            procedure='write_ensemble'))
+      return
+    end if
+
+    ! Write the data
+    call h5dwrite_f( &
+      dset_h, H5T_IEEE_F64LE, ensemble, data_block, ierr, &
+      file_space_id=dataspace)
+
+    if (ierr < 0) then
+      call throw(status, new_hdf5_exception(ierr, &
+                                            'HDF5 error writing data', &
+                                            filename=preassim_filename, &
+                                            procedure='write_ensemble'))
+      return
+    end if
+
+    ! Close the dataspace
+    call h5sclose_f(dataspace, ierr)
+
+    if (ierr < 0) then
+      call throw(status, new_hdf5_exception(ierr, &
+                                            'HDF5 error closing dataspace', &
+                                            filename=preassim_filename, &
+                                            procedure='write_ensemble'))
+      return
+    end if
+
+    ! Close the dataset
+    call h5dclose_f(dset_h, ierr)
+
+    if (ierr < 0) then
+      call throw(status, new_hdf5_exception(ierr, &
+                                            'HDF5 error closing dataset', &
+                                            filename=preassim_filename, &
+                                            procedure='write_ensemble'))
+      return
+    end if
+
+    ! Close the file
+    call h5fclose_f(h5file_h, ierr)
+
+    if (ierr < 0) then
+      call throw(status, new_hdf5_exception(ierr, &
+                                            'HDF5 error closing file', &
+                                            filename=preassim_filename, &
+                                            procedure='write_ensemble'))
+      return
+    end if
+
+  end subroutine write_ensemble
 
 end module advect1d_model_interface_tests
 
@@ -133,25 +161,31 @@ program test_advect1d_model_interface
   use model_interface_tests, ONLY: run_all
   use advect1d_assimilate_interfaces, ONLY: advect1d_interface, &
                                             new_advect1d_interface
-  use advect1d_model_interface_tests, ONLY: generate_inputs, run_all_advect1d
+  use advect1d_model_interface_tests, ONLY: write_ensemble, generate_ensemble, run_all_advect1d
   use system_mpi
   implicit none
   type(advect1d_interface)::model_interface
+  integer, parameter::istep = 1
   integer, parameter::n_observations = 5
   integer, parameter::state_size = 100
   integer, parameter::n_ensemble = 15
+  real(kind=8)::ensemble(n_ensemble, state_size)
   integer::rank, ierr
 
   call mpi_init(ierr)
 
-  model_interface = new_advect1d_interface( &
-                    n_ensemble, state_size, mpi_comm_world)
-
   call mpi_comm_rank(mpi_comm_world, rank, ierr)
 
-  if (rank == 0) call generate_inputs(model_interface, 20)
+  ensemble = generate_ensemble(istep, state_size, n_ensemble)
+
+  call MPI_Bcast(ensemble, state_size*n_ensemble, MPI_DOUBLE_PRECISION, 0, mpi_comm_world, ierr)
+
+  if (rank == 0) call write_ensemble(istep, ensemble)
 
   call mpi_barrier(mpi_comm_world, ierr)
+
+  model_interface = new_advect1d_interface( &
+                    n_ensemble, state_size, mpi_comm_world)
 
   call run_all(model_interface)
   call run_all_advect1d(model_interface)
