851cc2b2988011678a95609f1d0827b418c7f6cd
John Haiducek <jhaiduce@gmail.com>
2021-01-29 16:16:40 -0500

    Import system_mpi using find_package, and deleted redundant components

--- cmake/modules/lenkf_functions.cmake
+++ cmake/modules/lenkf_functions.cmake
@@ -2,25 +2,7 @@ cmake_minimum_required(VERSION 3.10)
 
 enable_language(Fortran)
 
-find_package(MPI REQUIRED)
-
-if(MPI_Fortran_HAVE_F90_MODULE)
-  add_definitions(-DHAVE_MPI_F90_MODULE)
-endif()
-
-if(MPI_Fortran_HAVE_F08_MODULE)
-  add_definitions(-DHAVE_MPI_F08_MODULE)
-endif()
-
 find_package(HDF5 COMPONENTS Fortran REQUIRED)
-
-function(configure_mpi_target target)
-  target_compile_definitions(${target} PRIVATE ${MPI_Fortran_COMPILE_DEFINITIONS})
-  target_compile_options(${target} PRIVATE ${MPI_Fortran_COMPILE_OPTIONS})
-  target_include_directories(${target} PRIVATE ${MPI_Fortran_INCLUDE_DIRS})
-  target_link_libraries(${target} PRIVATE ${MPI_Fortran_LINK_FLAGS})
-  target_link_libraries(${target} PRIVATE ${MPI_Fortran_LIBRARIES})
-endfunction()
 
 function(configure_hdf5_target target)
 
