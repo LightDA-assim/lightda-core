d0df40248612cc7e83de0eb2dc28bb443b81d8de
John Haiducek <jhaiduce@gmail.com>
2022-05-16 18:27:31 -0400

    Add a mask array and associated getter/setter procedures to the observation_set type

--- src/observations.F90
+++ src/observations.F90
@@ -1,12 +1,17 @@
 module observations
 
+  use exceptions, only: throw, new_exception, error_container
+
   implicit none
 
   type, abstract::observation_set
+    logical, allocatable, private::mask(:)
   contains
     procedure(get_size), deferred::get_size
     procedure(get_values), deferred::get_values
     procedure(get_values), deferred::get_errors
+    procedure::get_mask
+    procedure::set_mask
   end type observation_set
 
   abstract interface
@@ -70,5 +75,55 @@ module observations
     end function get_errors
 
   end interface
+
+contains
+
+  function get_mask(this, status) result(mask)
+
+    ! Arguments
+    class(observation_set), target::this
+         !! Observation set
+    type(error_container), intent(out), optional::status
+         !! Error status
+
+    ! Result
+    logical, pointer::mask(:)
+         !! Number of observation values in set
+
+    if (allocated(this%mask)) then
+      ! Mask already exists, just return the pointer
+      mask => this%mask
+      return
+    end if
+
+    ! Allocate the mask array
+    allocate (this%mask(this%get_size(status)))
+
+    ! Set all elements to .true. by default
+    this%mask = .true.
+
+    mask => this%mask
+
+  end function get_mask
+
+  subroutine set_mask(this, mask, status)
+
+    ! Arguments
+    class(observation_set)::this
+         !! Observation set
+    logical, intent(in)::mask(:)
+    type(error_container), intent(out), optional::status
+         !! Error status
+
+    if (size(mask) /= this%get_size()) then
+
+      call throw(status, new_exception( &
+                 'Size of mask differs from size of observation set.'))
+
+    end if
+
+    this%mask = mask
+
+  end subroutine set_mask
 
 end module observations
